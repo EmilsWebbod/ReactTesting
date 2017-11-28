2ae834157f230058fa549540e361363261ce3037
EmilsWebbod <emil@webbod.no>
2017-11-28 19:17:43 +0100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const express = require('express');
 
 // Create our app
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers('x-forwarded-proto') === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Server running on port: 3000');
-})
+app.listen(port, () => {
+  console.log('Server running on port: ' + PORT);
+});
